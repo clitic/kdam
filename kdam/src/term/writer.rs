a5f98b4c98165048c697f29edb40b31f8a2db263
Owen Shepherd <owen@owen.cafe>
2024-11-05 11:41:41 +0000

    Add tty mode

--- kdam/src/term/writer.rs
+++ kdam/src/term/writer.rs
@@ -1,20 +1,34 @@
-use std::io::{stderr, stdout, Result, Write};
+use std::{
+    fs::{File, OpenOptions},
+    io::{stderr, stdout, Result, Write},
+};
 
 /// Stderr and Stdout writer.
 #[derive(Debug, Clone)]
 pub enum Writer {
     Stderr,
     Stdout,
+    Tty,
 }
 
+#[cfg(target_os = "windows")]
+const TTY_PATH: &str = "CON";
+
+#[cfg(not(target_os = "windows"))]
+const TTY_PATH: &str = "/dev/tty";
+
 impl Writer {
-    /// Print text buffer in terminal followed by a flush.
-    pub fn print(&self, buf: &[u8]) -> Result<()> {
-        let mut writer: Box<dyn Write> = match self {
+    fn get_writer(&self) -> Result<Box<dyn Write>> {
+        Ok(match self {
             Self::Stderr => Box::new(stderr().lock()),
             Self::Stdout => Box::new(stdout().lock()),
-        };
+            Self::Tty => Box::new(OpenOptions::new().append(true).open(TTY_PATH)?),
+        })
+    }
 
+    /// Print text buffer in terminal followed by a flush.
+    pub fn print(&self, buf: &[u8]) -> Result<()> {
+        let mut writer: Box<dyn Write> = self.get_writer()?;
         writer.write_all(buf)?;
         writer.flush()?;
         Ok(())
@@ -23,7 +37,7 @@ impl Writer {
     /// Print text buffer in terminal followed by a flush at specified position.
     ///
     /// # Note
-    /// 
+    ///
     /// Cursor position is restored to original position after buffer is printed.
     ///
     /// # Example
@@ -34,10 +48,7 @@ impl Writer {
     /// Writer::Stderr.print_at(1, format!("1 + 1 = {}", 2).as_bytes()).unwrap();
     /// ```
     pub fn print_at(&self, position: u16, buf: &[u8]) -> Result<()> {
-        let mut writer: Box<dyn Write> = match self {
-            Self::Stderr => Box::new(stderr().lock()),
-            Self::Stdout => Box::new(stdout().lock()),
-        };
+        let mut writer: Box<dyn Write> = self.get_writer()?;
 
         if position > 0 {
             writer.write_all("\n".repeat(position as usize).as_bytes())?;
